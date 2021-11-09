bacc1c3a475ef6e72558b366fe5db458502cf254
GeorgeLMZN <georgeslmzn@gmail.com>
2021-11-09 05:10:29 +0400

    add navbar component, add topbar component, fonts, and normalize css file

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <TopBar />
+    <div class="header-bottom">
+      <nav class="header-nav">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+import TopBar from "./TopBar.vue";
+export default {
+  components: {
+    TopBar,
+  },
+};
+</script>
